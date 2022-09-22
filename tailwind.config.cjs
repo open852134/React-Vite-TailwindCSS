539f3070f55469d147151b02ffb57de0cb167999
Steven Weng <open852134@gmail.com>
2022-09-22 14:02:47 +0800

    feat: update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      xl: "1280px",
+    },
+
+    colors: {
+      almostWhite: "hsl(0, 0%, 98%)",
+      mediumGray: "hsl(0, 0%, 41%)",
+      almostBlack: "hsl(0, 0%, 8%)",
+    },
+    extend: {
+      boxShadow: {
+        navBar: "0px 0px 16px 3px rgba(204,204,204,0.7)",
+      },
+    },
   },
   plugins: [],
 };
